693c9d40d75e57ddcd1fefec0c680f2f63feb66e
Adrian Garmendia <dejima@gmail.com>
2020-08-24 14:27:46 -0500

    adding data

---
+++ bangagong.session.sql
@@ -0,0 +1,10 @@
+INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
+VALUES (1, 5);
+INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
+VALUES (2, 4);
+INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
+VALUES (3, 3);
+INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
+VALUES (4, 2);
+INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
+VALUES (5, 1);
