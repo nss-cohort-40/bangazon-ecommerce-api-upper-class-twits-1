204e9a13edba776d34094ee819ca43dd4ee9a6f6
tannerb9 <tanner-brainard@hotmail.com>
2020-08-24 11:17:47 -0500

    fix: deleted the ecommerceapi project within the bangazon project directory. created ecommerceapi app

--- ecommerceapi/models/customer.py
+++ ecommerceapi/models/customer.py
@@ -3,13 +3,14 @@ from django.db.models import F
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Customer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=50)
-    phone_number = models.PhoneNumberField()
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
 
 
 class Meta:
