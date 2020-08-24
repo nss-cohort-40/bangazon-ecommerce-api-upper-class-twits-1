79f41f549404ac95ecad457b9b9878ffdca73e06
tannerb9 <tanner-brainard@hotmail.com>
2020-08-24 13:43:36 -0500

    fix: added required attributes to decimal field and corrected fields on order_product

--- ecommerceapi/models/order_product.py
+++ ecommerceapi/models/order_product.py
@@ -5,8 +5,8 @@ from .product import Product
 
 class OrderProduct(models.Model):
 
-    product = models.models.ForeignKey(Product, on_delete=models.DO_NOTHING)
-    order = models.models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = ("order product")
