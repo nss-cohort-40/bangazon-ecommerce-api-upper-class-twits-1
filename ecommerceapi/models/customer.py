e35eac807410ff061f061b0a8b72327f312578b0
tannerb9 <tanner-brainard@hotmail.com>
2020-08-24 11:46:34 -0500

    fix: changed phone_number field type to integer field and correction import on urls.py

--- ecommerceapi/models/customer.py
+++ ecommerceapi/models/customer.py
@@ -3,15 +3,16 @@ from django.db.models import F
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Customer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=50)
-    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    phone_number = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
-class Meta:
+    class Meta:
     ordering = (F('user.date_joined').asc(nulls_last=True),)
