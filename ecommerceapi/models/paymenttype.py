b73b562e1e95cf6fd3ef86b8691e4be01c3d7767
leigharobinson <lmarobinson24@gmail.com>
2020-08-24 12:08:18 -0500

    I created paymenttype.py to build our table

---
+++ ecommerceapi/models/paymenttype.py
@@ -0,0 +1,19 @@
+from django.db import models
+from .customer import Customer
+
+# Create your models here.
+
+
+class PaymentType(models.Model):
+    merchant_name = models.CharField(max_length=50)
+    account_number = models.CharField(max_length=50)
+    customer_id = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    expiration_date = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    class Meta:
+        verbose_name = ("paymenttype")
+        verbose_name_plural = ("paymenttypes")
+
+    def __str__(self):
+        return f'{self.merchant_name} {self.account_number}'
