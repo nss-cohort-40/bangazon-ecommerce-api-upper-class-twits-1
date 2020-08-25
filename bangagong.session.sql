17d82e7738f8c454af916906a4cc0fa8b6a8ffbb
Adrian Garmendia <dejima@gmail.com>
2020-08-25 09:34:36 -0500

    created dummy data

--- bangagong.session.sql
+++ bangagong.session.sql
@@ -1,10 +1,8 @@
-INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
-VALUES (1, 5);
-INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
-VALUES (2, 4);
-INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
-VALUES (3, 3);
-INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
-VALUES (4, 2);
-INSERT INTO ecommerceapi_orderproduct (order_id, product_id)
-VALUES (5, 1);
+INSERT INTO ecommerceapi_paymenttype
+    (merchant_name, account_number, customer_id, expiration_date, created_date)
+VALUES
+    ("Credit Card", 1002345678, 1, "2020-12-01", "2020-01-01"),
+    ("Debit Card", 1002345678, 2, "2020-12-02", "2020-01-01"),
+    ("Credit Card", 1002345678, 3, "2020-12-03", "2020-01-01"),
+    ("Debit Card", 1002345678, 4, "2020-12-04", "2020-01-01"),
+    ("Credit Card", 1002345678, 5, "2020-12-05", "2020-01-01");
