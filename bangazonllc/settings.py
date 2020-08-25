64cb19239142493b48698167c42fabcd8c37ab96
leigharobinson <lmarobinson24@gmail.com>
2020-08-25 09:29:05 -0500

    commented out auth in settings.py

--- bangazonllc/settings.py
+++ bangazonllc/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
