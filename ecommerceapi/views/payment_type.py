44e01e5420e190175ace24a3dfb48b0d6e27b57e
leigharobinson <lmarobinson24@gmail.com>
2020-08-26 09:56:28 -0500

    fixing bug with launching runserver

--- ecommerceapi/views/payment_type.py
+++ ecommerceapi/views/payment_type.py
@@ -4,7 +4,6 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from rest_framework.decorators import action
 from ecommerceapi.models import Customer, PaymentType
 
 
